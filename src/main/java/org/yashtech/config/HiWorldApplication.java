fa1ec087ca9466db5efe0effbcba57945421cc76
ishatiwari26 <42269541+ishatiwari26@users.noreply.github.com>
2018-12-18 12:55:00 +0530

    HiWorldApplication.java
    HiWorldApplication.java

---
+++ src/main/java/org/yashtech/config/HiWorldApplication.java
@@ -0,0 +1,14 @@
+package org.yashtech.config;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HiWorldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HiWorldApplication.class, args);
+	}
+
+}
+
